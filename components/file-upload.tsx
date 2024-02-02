bdbfadac9d01de01793b3cd142a5cf5b3b8cf4ad
srisurya6669 <srisurya6669@gmail.com>
2024-02-02 21:28:17 +0530

    Added image uploads for server

---
+++ components/file-upload.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { UploadDropzone } from "@/lib/uploadthing";
+import "@uploadthing/react/styles.css";
+import { X } from "lucide-react";
+import Image from "next/image";
+
+interface FileUploadProps {
+  onChange: (url?: string) => void;
+  value: string;
+  endpoint: "messageFile" | "serverImage";
+}
+
+export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
+  const fileTyle = value.split(".").pop();
+  if (value && fileTyle !== "pdf") {
+    return (
+      <div className="relative h-20 w-20">
+        <Image fill src={value} alt="upload" className="rounded-full" />
+        <button
+          className="bg-rose-400 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+          type="button"
+          onClick={() => onChange("")}
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
+  return (
+    <UploadDropzone
+      endpoint={endpoint}
+      onClientUploadComplete={(res) => {
+        onChange(res?.[0].url);
+      }}
+      onUploadError={(error: Error) => {
+        console.log(error);
+      }}
+    />
+  );
+};
